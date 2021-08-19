14659a607594cc51de85787d639bcefdf010935b
alexandraalexa <gubanovasasa@gmail.com>
2021-08-19 12:41:01 +0300

    js

---
+++ index.js
@@ -0,0 +1,7 @@
+//alert ("test!") ;
+//let  vorname= prompt("Как тебя зовут?");
+//alert (`Привет ${vorname}!`);
+function showMessage(){
+    let  vorname= prompt("Как тебя зовут?");
+    alert (`Привет ${vorname}!`);
+}
