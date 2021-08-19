7ba26619646ea8ff971213086459ab708b6dad24
alexandraalexa <gubanovasasa@gmail.com>
2021-08-19 17:48:26 +0300

    калькулятор

--- index.js
+++ index.js
@@ -5,3 +5,41 @@ function showMessage(){
     let  vorname= prompt("Как тебя зовут?");
     alert (`Привет ${vorname}!`);
 }
+//function showMessage2(){
+ //  let a= prompt(`выберете чиcло`);
+ //  let b=prompt(`выберете следующее чиcло`);
+// return a-b;
+//}
+// alert(showMessage2(`${a}-${b}`))$
+
+let op;
+function func(){
+  let result;
+  let num1=Number(document.getElementById(`num1`).value);
+let num2= Number(document.getElementById(`num2`).value)
+  switch (op){
+case`+`:
+result=num1+num2;
+break;
+case`-`:
+result=num1-num2;
+break;
+case`*`:
+result=num1*num2;
+break;
+case`/`:
+if(num2){
+  result=num1/num2;
+}
+else{result=`бесконечность`}
+break;
+  }
+
+  document.getElementById(`result`).innerHTML=result;
+
+
+}
+
+
+
+
